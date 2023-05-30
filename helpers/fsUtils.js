a264b19f9a4038f276d1fa78f6a95c3c38962469
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-05-30 12:56:51 -0700

    Added the addNote Function on the fsUtil Class and setup the POST route for /api/notes

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -2,18 +2,36 @@ class FsUtils {
   constructor() {
     this.fs = require('fs');
     this.path = require('path');
+    this.dbPath = this.path.join(__dirname, '..', 'db', 'db.json');
+    this.notes = this.getNotes();
   }
 
   getNotes() {
     try {
-      const data = this.fs.readFileSync(
-        this.path.join(__dirname, '..', 'db', 'db.json'),
-        'utf-8',
-      );
+      const data = this.fs.readFileSync(this.dbPath, 'utf-8');
       return JSON.parse(data);
     } catch (error) {
       return error;
     }
+  }
+  addNote(note) {
+    try {
+      this.notes.push(note);
+      this.fs.writeFileSync(this.dbPath, JSON.stringify(this.notes, null, 2));
+      return true;
+    } catch (error) {
+      return error;
+    }
+  }
+  /***
+   * Source for this code: from the 11-Express/01-Activities/Day3/22-Stu-Modular-Routing/Solved/helpers/uuid.js file
+   *
+   * @summary Generates a random id of 4 characters with random letters and numbers
+   */
+  uuid() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(2);
   }
 }
 
--- routes/notes.js
+++ routes/notes.js
@@ -12,4 +12,21 @@ notes.get('/', (req, res) => {
   return res.status(200).json(fs.getNotes());
 });
 
+notes.post('/', (req, res) => {
+  const { title, text } = req.body;
+  if (!title || !text) {
+    return res.status(400).json({ msg: 'Please include title and text' });
+  }
+  const newNote = {
+    title,
+    text,
+    id: fs.uuid(),
+  };
+  const success = fs.addNote(newNote);
+  if (success instanceof Error) {
+    return res.status(500).json(success);
+  }
+  return res.status(201).json(success);
+});
+
 module.exports = notes;
