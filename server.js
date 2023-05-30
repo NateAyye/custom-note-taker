b28514e92469dc57e9bebcba42dbc98557f3a76a
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-05-30 12:34:42 -0700

    Base files for the notes route were added and linked to the server file

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use('/api', require('./routes'));
+
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
