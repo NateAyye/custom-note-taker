de874cea820374d98b345b818944c1f3eafe6275
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-05-30 12:32:16 -0700

    Added the notes route that directs you to the notes.html page

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
 
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
